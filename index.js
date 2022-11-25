49e7280960aa7b9d5d1f787f15ec553b5ce641b6
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2022-11-24 20:50:07 -0500

    Bot Startup

---
+++ index.js
@@ -0,0 +1,18 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Config .env values
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Bot is ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
